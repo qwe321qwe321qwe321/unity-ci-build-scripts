15cfbf0711e942975cc96236604da03b8a49cca8
qwe321qwe321qwe321 <aegis321123@gamil.com>
2020-02-13 17:59:04 +0800

    fix

--- ci/build.sh
+++ ci/build.sh
@@ -5,7 +5,7 @@ set -x
 
 echo "Building for $BUILD_TARGET"
 
-export BUILD_PATH=${PROJECT_PATH}Builds/$BUILD_TARGET/
+export BUILD_PATH=/root/project/${PROJECT_PATH}Builds/$BUILD_TARGET/
 mkdir -p $BUILD_PATH
 
 ${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
