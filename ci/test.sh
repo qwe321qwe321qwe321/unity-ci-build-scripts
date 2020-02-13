02f6fc2c85b48f250df4bd8b17ef968bff78bd77
qwe321qwe321qwe321 <aegis321123@gamil.com>
2020-02-13 19:07:51 +0800

    fix

--- ci/test.sh
+++ ci/test.sh
@@ -16,17 +16,13 @@ UNITY_EXIT_CODE=$?
 
 if [ $UNITY_EXIT_CODE -eq 0 ]; then
   echo "Run succeeded, no failures occurred";
+  cat /root/project/${PROJECT_PATH}$TEST_PLATFORM-results.xml | grep test-run | grep Passed
 elif [ $UNITY_EXIT_CODE -eq 2 ]; then
   echo "Run succeeded, some tests failed";
+  cat /root/project/${PROJECT_PATH}$TEST_PLATFORM-results.xml | grep test-run | grep Failed
 elif [ $UNITY_EXIT_CODE -eq 3 ]; then
   echo "Run failure (other failure)";
 else
   echo "Unexpected exit code $UNITY_EXIT_CODE";
 fi
-pwd
-ls
-ls /
-ls /root/
-ls /root/project/
-cat /root/project/${PROJECT_PATH}$TEST_PLATFORM-results.xml | grep test-run | grep Passed
 exit $UNITY_TEST_EXIT_CODE
